e269f7b2c82c22ebb7f664780751f90629ab817d
Minh Tan Vu <vuminhtan.nuce3@gmail.com>
2023-07-07 15:02:42 +0700

    init project

---
+++ Travelify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travelify
+//
+//  Created by Minh Tan Vu on 07/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
